0aa1385ba55e5b994c0af8c79ff20ed705ee5f35
PiXy <pixy@Admins-MacBook-Pro.local>
2017-05-29 14:35:02 +0530

    reset

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -22,7 +22,7 @@ var all = {
   port: process.env.PORT || 9000,
 
   // Server IP
-  ip: 'https://cccchallengedashboard.herokuapp.com/',
+  ip: process.env.IP || '0.0.0.0',
 
   // Should we populate the DB with sample data?
   seedDB: false,
--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -18,6 +18,6 @@ module.exports = {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://heroku_dtpcc18z:l3oo03ja6mms2gicfgql703hrb@ds143911-a0.mlab.com:43911,ds143911-a1.mlab.com:43911/heroku_dtpcc18z?replicaSet=rs-ds143911'
+            'mongodb://localhost/fccuserlist'
   }
 };
