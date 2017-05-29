7f3c9bdb9fdc20c9306b9f60ffedb73832e66297
PiXy <pixy@Admins-MacBook-Pro.local>
2017-05-29 14:43:27 +0530

    e

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -4,10 +4,6 @@
 // =================================
 module.exports = {
   // Server IP
-
-
-  DOMAIN:   'https://cccchallengedashboard.herokuapp.com',
-
   ip:       process.env.OPENSHIFT_NODEJS_IP ||
             process.env.IP ||
             undefined,
@@ -19,6 +15,6 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri:    'mongodb://heroku_dtpcc18z:l3oo03ja6mms2gicfgql703hrb@ds143911-a0.mlab.com:43911,ds143911-a1.mlab.com:43911/heroku_dtpcc18z?replicaSet=rs-ds143911'
-  }
+    uri: 'mongodb://heroku_dtpcc18z:l3oo03ja6mms2gicfgql703hrb@ds143911-a0.mlab.com:43911,ds143911-a1.mlab.com:43911/heroku_dtpcc18z?replicaSet=rs-ds143911'
+  },
 };
